c4d61a2fc3b9f33830bf6cba54ceeec113326abe
potados99 <potados99@gmail.com>
2019-06-23 16:57:44 +0900

    Update

--- nodes/apricot/wrapper.cpp
+++ nodes/apricot/wrapper.cpp
@@ -6,65 +6,58 @@
 
 WiFiUDP udp;
 CoapServer server(udp);
+Device myLED("MyLED", 2);
 
-Device myDevice("MyDevice", 2);
-Button myButton(0, HIGH, buttonClicked); /* pin 0, pull-up, callback */
+char *light_callback(CoapPacket &packet, IPAddress ip, int port);
 
 void setup() {
-  if (! WiFi.getAutoConnect()) {
-      WiFi.begin(SSID, PASSWORD); /* in wifi.h */
+  Serial.begin(115200);
+  
+  WiFi.begin(MY_SSID, MY_PASSWORD);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
 
-  server.addResource(light_callback, "light");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // pinMode(3, FUNCTION_3);
+
+  server.addResource(light_callback, "LED");
 
   server.start();
 }
 
 void loop() {
   server.loop();
-  myButton.loop();
-}
-
-
-void buttonClicked() {
-  myDevice.togglePower();
 }
 
 char *light_callback(CoapPacket &packet, IPAddress ip, int port) {
-  Serial.println("BGN");
 
   const char * msg = (const char *)packet.payload ? (const char *)packet.payload : "NULL";
   bool methodIsGet = (packet.code == COAP_GET);
-
-  Serial.print("Method:");
-  Serial.println(methodIsGet ? "GET" : "PUT");
-
-  Serial.print("Payload:");
-  Serial.println(msg ? msg : "NULL");
 
   char *reply = (char *)malloc(32);
   memset(reply, 0, 32);
 
   if (methodIsGet) {
-    sprintf(reply, "Power is %s!", MyRelay.getPower() ? "On" : "Off");
+    sprintf(reply, "Power is %s!", myLED.getPower() ? "On" : "Off");
   }
   else {
     if (strcmp(msg, "on") == 0) {
-      MyRelay.setPower(true);
+      myLED.setPower(true);
       sprintf(reply, "Turned On :)");
     }
     else if (strcmp(msg, "off") == 0) {
-      MyRelay.setPower(false);
+      myLED.setPower(false);
       sprintf(reply, "Turned Off :(");
     }
     else {
       sprintf(reply, "I don't get it \"%s\"..sorry", msg);
     }
   }
-
-  Serial.print("Reply:");
-  Serial.println(reply);
-  Serial.println("END");
 
   return reply;
 }
